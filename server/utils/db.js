41887e78a22ef432fa96c3bef6ca9404464a1065
Por <Poramaporn2540@gmail.com>
2024-10-13 18:32:53 +0700

    feat(db): set up database connection

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,9 @@
 // Set up db connection here
+
+import { mongoClient } from mongodb;
+
+const connectionString = 'mongodb://127.0.0.1:27017';
+
+export const client = new mongoClient(connectionString, {useUnifiedTopology: true});
+export const db = client.db;
+
