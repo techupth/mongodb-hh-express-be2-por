72d333fb42f8c1e89264e3b532f2cf31e80e0310
Por <Poramaporn2540@gmail.com>
2024-10-13 18:54:21 +0700

    refactor(db): refactor set up code for database connection

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,9 +1,23 @@
 // Set up db connection here
-
-import { mongoClient } from mongodb;
+import { MongoClient } from 'mongodb';
 
 const connectionString = 'mongodb://127.0.0.1:27017';
+const dbName = 'practice-mongo'
 
-export const client = new mongoClient(connectionString, {useUnifiedTopology: true});
-export const db = client.db;
+export let db;
 
+export const client = new MongoClient(connectionString, {useUnifiedTopology: true});
+
+export async function connectDatabase() {
+    try {
+        //connect to 
+        await client.connect();
+        console.log('successfully connected to mongoDB');
+
+        //assign db object after connection successful
+         db = client.db(dbName);
+    } catch (error) {
+        console.log('Error connecting to mongoDB', error);
+        throw error;
+    }
+}
